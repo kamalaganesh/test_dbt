b0d123444639376dea279a540dba5c92939b9eab
Kamalaganesh Chellappa <kamalaganesh.c@gmail.com>
2024-04-07 22:17:00 +0000

    Fix error

--- models/staging/tpch/stg_tpch_line_items.sql
+++ models/staging/tpch/stg_tpch_line_items.sql
@@ -1,36 +1,32 @@
-with source as (
+with
+    source as (select * from {{ source("tpch", "lineitem") }}),
 
-    select * from {{ source('tpch', 'lineitem') }}
+    renamed as (
 
-),
+        select
 
-renamed as (
+            {{ dbt_utils.surrogate_key(["l_orderkey", "l_linenumber"]) }}
+            as order_item_key,
+            l_orderkey as order_key,
+            l_partkey as part_key,
+            l_suppkey as supplier_key,
+            l_linenumber as line_number,
+            l_quantity as quantity,
+            l_extendedprice as extended_price,
+            l_discount as discount_percentage,
+            l_tax as tax_rate,
+            l_returnflag as return_flag,
+            l_linestatus as status_code,
+            l_shipdate as ship_date,
+            l_commitdate as commit_date,
+            l_receiptdate as receipt_date,
+            l_shipinstruct as ship_instructions,
+            l_shipmode as ship_mode,
+            l_comment as comment
 
-    select
-    
-        {{ dbt_utils.surrogate_key(
-            ['l_orderkey', 
-            'l_linenumber']) }}
-                as order_item_key,
-        l_orderkey as order_key,
-        l_partkey as part_key,
-        l_suppkey as supplier_key,
-        l_linenumber as line_number,
-        l_quantity as quantity,
-        l_extendedprice as extended_price,
-        l_discount as discount_percentage,
-        l_tax as tax_rate,
-        l_returnflag as return_flag,
-        l_linestatus as status_code,
-        l_shipdate as ship_date,
-        l_commitdate as commit_date,
-        l_receiptdate as receipt_date,
-        l_shipinstruct as ship_instructions,
-        l_shipmode as ship_mode,
-        l_comment as comment
+        from source
 
-    from source
+    )
 
-)
-
-select * from renamed
+select *
+from renamed
